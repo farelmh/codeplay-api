53895100abb4de98790b5d7cb82775f711787e2c
farelmh <farelmh01@gmail.com>
2025-11-10 23:57:56 +0700

    Update Courses.java

--- src/main/java/com/codeplay/codeplay_api/entity/Courses.java
+++ src/main/java/com/codeplay/codeplay_api/entity/Courses.java
@@ -24,7 +24,7 @@ public class Courses {
 
     private String deskripsi;
 
-    @OneToMany(mappedBy = "courses", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Lesson> lessons;
 }

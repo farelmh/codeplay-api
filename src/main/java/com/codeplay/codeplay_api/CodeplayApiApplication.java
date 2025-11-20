e572df4934fb0c6fb25dd0196bd1e0b41def53f5
farelmh <farelmh01@gmail.com>
2025-11-21 00:13:30 +0700

    Set default timezone to Asia/Jakarta in application initialization

--- src/main/java/com/codeplay/codeplay_api/CodeplayApiApplication.java
+++ src/main/java/com/codeplay/codeplay_api/CodeplayApiApplication.java
@@ -1,11 +1,20 @@
 package com.codeplay.codeplay_api;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CodeplayApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodeplayApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
 	}
 }
